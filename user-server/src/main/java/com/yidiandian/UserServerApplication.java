20989c20e9eca6a4dc5f26ea6dd9109df4b01fd6
luoxian <15290810931@163.com>
2018-12-04 23:02:30 +0800

    第一版:创建基本服务：包括服务注册中心

---
+++ user-server/src/main/java/com/yidiandian/UserServerApplication.java
@@ -0,0 +1,12 @@
+package com.yidiandian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServerApplication.class, args);
+	}
+}
