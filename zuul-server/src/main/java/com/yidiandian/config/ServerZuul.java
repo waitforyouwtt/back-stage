ed7c1ccd8cb532dca9fe6ef4be5dd7af70df6021
luoxian <15290810931@163.com>
2018-12-16 11:41:03 +0800

    第二版：引入网关

---
+++ zuul-server/src/main/java/com/yidiandian/config/ServerZuul.java
@@ -0,0 +1,56 @@
+package com.yidiandian.config;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * @Author: 一点点
+ * @Date: 2018/12/9 19:42
+ * @Version 1.0
+ */
+@Component
+public class ServerZuul extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(ServerZuul.class);
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null){
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){
+                return null;
+            }
+            log.info("ok");
+        }
+        return null;
+    }
+}
